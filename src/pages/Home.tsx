c6065a03c63ac6a54ef907379aef8fd528a24ddc
Santiago Papa <papa.santiago321@gmail.com>
2025-09-29 10:28:20 -0300

    Icons, reorganize structure and education

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,6 +12,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import ProfessionalBackground from "@/components/Background";
@@ -43,6 +44,8 @@ const toolbox = [
 const jobs = [
   {
     title: "Full Stack Developer",
+    logo: "https://carpinchodev.com/_ipx/q_80/logo.svg",
+    link: "https://carpinchodev.com/",
     company: "Carpincho Dev",
     period: "May 2024 - Present",
     description:
@@ -50,6 +53,8 @@ const jobs = [
   },
   {
     title: "Programming Freelancer",
+    logo: "https://cheyennevc.com/wp-content/uploads/2024/10/Scale.jpg",
+    link: "https://scale.com/",
     company: "Scale AI",
     period: "Feb 2024 - May 2024",
     description:
@@ -58,6 +63,7 @@ const jobs = [
   {
     title: "IoT Founder",
     company: "Dash Home",
+    logo: "/LogoDashhome.png",
     period: "Jul 2022 - Aug 2025",
     description:
       "Founded company providing custom home security and automation systems with comprehensive security implementations.",
@@ -91,6 +97,143 @@ const projects = [
   },
 ];
 
+const studies = {
+  title: "Studies",
+  data: [
+    {
+      title: "Information Systems Engineering",
+      institution: "Catholic University of Argentina",
+      logo: "https://uca.edu.ar/assets/img/UCA-Logo-3.png",
+      link: "https://uca.edu.ar/",
+      period: "Jan 2021 - Dec 2026",
+      skills: [
+        "Python Computing",
+        "Web Development",
+        "Communication Networks",
+        "Structured Programming",
+        "Business Administration",
+        "Object Oriented Programming",
+        "Statistics",
+        "Software Engineering",
+        "Advanced Physics",
+        "Advanced Calculus",
+        "Algebra",
+        "Software Design",
+        "Numeric Methods",
+        "Internet Protocols",
+        "Discrete Mathematics",
+        "Data Structures",
+        "Operating Systems",
+        "Algorithms & Computer Logic",
+        "Relational Databases",
+        "Mobile Applications",
+        "Artificial Intelligence",
+        "Models & Simulations",
+        "Parallel Programming & Clusters",
+        "Mathematical Programming",
+        "Introduction to Quantum Computing",
+      ],
+    },
+    {
+      title: "Data Engineering",
+      institution: "AWS Academy",
+      logo: "https://images.credly.com/size/160x160/images/8a28a66c-151d-4f2d-b021-ca7d3e146437/blob",
+      link: "https://www.credly.com/badges/6f5b95f1-edfd-459e-8eb2-8926712377a5/public_url",
+      period: "Jan 2025 - June 2025",
+      skills: [
+        "Data Pipelines",
+        "Data Engineering",
+        "Data Security",
+        "Data Governance",
+        "S3",
+        "Athena",
+        "Cloud 9",
+        "IAM",
+        "Cloud Formation",
+        "Glue",
+        "Redshift",
+        "EC2",
+        "EMR",
+        "Kinesis",
+        "Cloud Watch",
+        "Step Functions",
+      ],
+    },
+    {
+      title: "Introduction to Cybersecurity",
+      institution: "Cisco Networking Academy",
+      logo: "https://images.credly.com/size/160x160/images/af8c6b4e-fc31-47c4-8dcb-eb7a2065dc5b/I2CS__1_.png",
+      link: "https://www.credly.com/badges/7025bcd5-6587-4a77-97fc-b2ae2f510792/public_url",
+      period: "Aug 2025",
+      skills: [
+        "Network Vulnerabilities",
+        "Cybersecurity",
+        "Threat Detection",
+        "Privacy And Data Confidentiality",
+        "Cyber Best Practices",
+      ],
+    },
+    {
+      title: "Networking Basics",
+      institution: "Cisco Networking Academy",
+      logo: "https://images.credly.com/size/160x160/images/5bdd6a39-3e03-4444-9510-ecff80c9ce79/image.png",
+      link: "https://www.credly.com/badges/a242c658-0b98-4c01-a9f3-4394f9bf74bb/public_url",
+      period: "Aug 2025",
+      skills: [
+        "IPv4 Addresses",
+        "Application Layer Services",
+        "Network Media",
+        "Wireless Access",
+        "Protocols Standards",
+        "Network Types",
+      ],
+    },
+    {
+      title: "Networking Devices and Configuration",
+      institution: "Cisco Networking Academy",
+      logo: "https://images.credly.com/size/160x160/images/88316fe8-5651-4e61-a6be-5be1558f049e/image.png",
+      link: "https://www.credly.com/badges/091cdb83-135c-4075-bad0-88a03c6dbf98/public_url",
+      period: "Aug 2025",
+      skills: [
+        "Ethernet Operates",
+        "Cisco IOS",
+        "Binary Systems",
+        "Transport Layer Protocols",
+        "DHCP",
+        "Network Layer Protocols",
+        "Cisco Devices",
+        "DNS",
+        "ARP",
+        "Virtualization and Cloud Services",
+        "IPv4 Subnetting",
+        "Hierarchical Network Design",
+      ],
+    },
+    {
+      title: "Endpoint Security",
+      institution: "Cisco Networking Academy",
+      logo: "https://images.credly.com/size/160x160/images/0ca5f542-fb5e-4a22-9b7a-c1a1ce4c3db7/EndpointSecurity.png",
+      link: "https://www.credly.com/badges/edfe0a25-91a6-4995-900e-b886409c7f2a/public_url",
+      period: "Sep 2025",
+      skills: [
+        "Application Security",
+        "Antimalware Protection",
+        "Common Cyber Threats",
+        "Defending Systems And Devices",
+        "Host-based Intrusion Prevention",
+        "IP/TCP/UDP Vulnerabilities",
+        "Linux Basics",
+        "Mitigating Common Network Attacks",
+        "Network Security Infrastructure",
+        "Securing WLANs",
+        "System And Endpoint Protection",
+        "Windows Security",
+        "Wireless And Mobile Device Attacks",
+      ],
+    },
+  ],
+};
+
 const hero = {
   title: "Software Engineer & Security Enthusiast",
   description: `I specialize in full-stack software development, secure application
@@ -98,18 +241,6 @@ const hero = {
               Passionate about building robust digital solutions using modern
               development patterns while implementing comprehensive security
               measures throughout the software development lifecycle `,
-  about: `With over 2 years of experience in software development and
-                  cybersecurity, I've worked across web development and IoT
-                  security helping organizations build secure digital solutions.
-                  Currently pursuing my Bachelor's in Informatics Engineering
-                  with an expected graduation in December 2026, I'm just a few
-                  courses away from completing my degree. My expertise spans
-                  from full-stack application development to implementing
-                  comprehensive security measures. I hold certifications in AWS
-                  Data Engineering and Cisco Cybersecurity (Introduction to
-                  Cybersecurity, Endpoint Security, and Networking), and I'm
-                  constantly expanding my knowledge in secure development
-                  practices and emerging technologies.`,
 };
 
 const recentPosts = [
@@ -159,18 +290,46 @@ export default function Home() {
             </p>
           </section>
 
-          {/* About Me */}
+          {/* Jobs */}
           <section>
             <h2 className="text-2xl font-semibold text-foreground mb-6">
-              About Me
+              Experience
             </h2>
-            <Card className="hover:shadow-lg transition-shadow">
-              <CardContent className="px-6">
-                <p className="text-muted-foreground leading-relaxed">
-                  {hero.about}
-                </p>
-              </CardContent>
-            </Card>
+            <div className="space-y-4">
+              {jobs.map((job, index) => (
+                <Card
+                  key={index}
+                  className={`hover:shadow-lg transition-shadow ${job.link ? "cursor-pointer" : ""}`}
+                  onClick={() =>
+                    job.link ? window.open(job.link, "_blank") : null
+                  }
+                >
+                  <CardHeader>
+                    <div className="flex items-start">
+                      <img src={job.logo} alt={job.title} className="w-8 h-8" />
+                      <div className="ml-3">
+                        <CardTitle className="text-lg">{job.title}</CardTitle>
+                        <CardDescription className="text-base">
+                          {job.company}
+                        </CardDescription>
+                      </div>
+                      <Badge className="ml-auto" variant="outline">
+                        {job.period}
+                      </Badge>
+                    </div>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-muted-foreground">{job.description}</p>
+                  </CardContent>
+                </Card>
+              ))}
+              <div
+                data-iframe-width="150"
+                data-iframe-height="270"
+                data-share-badge-id="edfe0a25-91a6-4995-900e-b886409c7f2a"
+                data-share-badge-host="https://www.credly.com"
+              ></div>
+            </div>
           </section>
 
           {/* Toolbox */}
@@ -191,27 +350,40 @@ export default function Home() {
             </Card>
           </section>
 
-          {/* Jobs */}
+          {/* Education */}
           <section>
             <h2 className="text-2xl font-semibold text-foreground mb-6">
-              Experience
+              {studies.title}
             </h2>
             <div className="space-y-4">
-              {jobs.map((job, index) => (
-                <Card key={index} className="hover:shadow-lg transition-shadow">
+              {studies.data.map((degree, index) => (
+                <Card
+                  key={index}
+                  className="hover:shadow-lg hover:cursor-pointer transition-shadow"
+                  onClick={() => window.open(degree.link, "_blank")}
+                >
                   <CardHeader>
-                    <div className="flex justify-between items-start">
-                      <div>
-                        <CardTitle className="text-lg">{job.title}</CardTitle>
-                        <CardDescription className="text-base">
-                          {job.company}
-                        </CardDescription>
+                    <div className="flex items-start">
+                      <div className="w-12 h-12">
+                        <img src={degree.logo} alt="Logo" />
                       </div>
-                      <Badge variant="outline">{job.period}</Badge>
+                      <CardTitle className="text-lg flex-row ml-2">
+                        <div className="text-foreground">{degree.title}</div>
+                        <div className="text-muted-foreground">
+                          {degree.institution}
+                        </div>
+                      </CardTitle>
+                      <CardDescription className="text-base ml-auto">
+                        {degree.period}
+                      </CardDescription>
                     </div>
                   </CardHeader>
                   <CardContent>
-                    <p className="text-muted-foreground">{job.description}</p>
+                    {degree.skills.map((skill, index) => (
+                      <Badge className="m-1" variant="outline">
+                        {skill}
+                      </Badge>
+                    ))}
                   </CardContent>
                 </Card>
               ))}
